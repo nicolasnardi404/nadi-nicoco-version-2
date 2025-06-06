400d699b582c35e00789009a7bb7d205b5c5eee9
esquizo ciborgue <nicolasnardi404@gmail.com>
2025-06-06 10:59:45 +0200

    add background

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,14 +2,17 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import styled from "styled-components";
-import { header } from "./index.module.css";
 
 const Title = styled.header`
   `
 
+interface HeaderProps {
+  siteTitle: string;
+}
+
 // text-shadow: 19px 34px 0px #1f16e5, -2px -1px 0px #e51616, 14px 24px 0px #e51616;
-const Header = ({ siteTitle }) => (
-  <Title className={header}>
+const Header: React.FC<HeaderProps> = ({ siteTitle }) => (
+  <Title>
     <Link
       to="/"
       style={{
